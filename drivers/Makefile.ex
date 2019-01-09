50cf33a3594793806c77306a8d7fe1cfd42c7af4
Rachel Krohn <rkrohn@nd.edu>
2019-01-09 17:14:36 -0500

    makefile fixes - only compile drivers if they've changed

--- drivers/Makefile.ex
+++ drivers/Makefile.ex
@@ -1,9 +1,9 @@
-MAIN = node2vec
-DEPH = $(DRVSNAPADV)/n2v.h $(DRVSNAPADV)/word2vec.h $(DRVSNAPADV)/biasedrandomwalk.h
-DEPCPP = $(DRVSNAPADV)/n2v.cpp $(DRVSNAPADV)/word2vec.cpp $(DRVSNAPADV)/biasedrandomwalk.cpp
-CXXFLAGS += $(CXXOPENMP)
+MAINNODE = node2vec
+DEPHNODE = $(DRVSNAPADV)/n2v.h $(DRVSNAPADV)/word2vec.h $(DRVSNAPADV)/biasedrandomwalk.h
+DEPCPPNODE = $(DRVSNAPADV)/n2v.cpp $(DRVSNAPADV)/word2vec.cpp $(DRVSNAPADV)/biasedrandomwalk.cpp
 
-MAIN = word2vec
-DEPH = $(DRVSNAPADV)/word2vec.h
-DEPCPP = $(DRVSNAPADV)/word2vec.cpp
+MAINWORD = word2vec
+DEPHWORD = $(DRVSNAPADV)/word2vec.h
+DEPCPPWORD = $(DRVSNAPADV)/word2vec.cpp
+
 CXXFLAGS += $(CXXOPENMP)
